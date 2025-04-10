cfbbe9e9256ba129533621fa6d01b1bee6e048cc
ddhanawade <deepak_dhanawade@intuit.com>
2025-04-10 18:27:50 +0530

    Css And Html Changes

---
+++ public/mainscript.js
@@ -0,0 +1,19 @@
+ // Mobile menu toggle
+ document.getElementById('mobile-menu').addEventListener('click', function() {
+    var navUl = document.querySelector('nav ul');
+    navUl.classList.toggle('show');
+});
+
+// Add active class on navigation links
+const currentLocation = location.pathname;
+const menuItems = document.querySelectorAll('.nav-link');
+menuItems.forEach(item => {
+    if (item.getAttribute('href') === currentLocation) {
+        item.classList.add('active');
+    }
+
+    item.addEventListener('click', function() {
+        menuItems.forEach(link => link.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
