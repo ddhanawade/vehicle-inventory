4099767df5e42b366b5fc8b6a0c4b95f6e8159ea
ddhanawade <ddhanawade804@gmail.com>
2025-07-02 18:59:05 +0530

    Update AuthInterceptor.ts

--- src/app/services/AuthInterceptor.ts
+++ src/app/services/AuthInterceptor.ts
@@ -1,8 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
-  // const excludedPattern = /^(https:\/\/auth\.fleet-manager\.in\/auth\/[^/]+|https:\/\/inventory-service-prd\.us-east-2\.elasticbeanstalk\.com)/;
-  const excludedPattern = /^(https:\/\/auth\.fleet-manager\.in\/[^/]+|https:\/\/fleet-manager\.in\/[^/]+|https:\/\/api\.fleet-manager\.in)/;
+  const excludedPattern = /^(https:\/\/auth\.fleet-manager\.in\/[^/]+|https:\/\/fleet-manager\.in\/[^/]+|https:\/\/api\.fleet-manager\.in|http:\/\/52\.66\.207\.66:8081)/;
 
   if (!excludedPattern.test(req.url)) {
     const token = localStorage.getItem('authToken');
