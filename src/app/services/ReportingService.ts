e7cf0d3504aaf46f991cb609d0ab44ea8750a441
ddhanawade <deepak_dhanawade@intuit.com>
2025-07-16 22:37:01 +0530

    test api added

--- src/app/services/ReportingService.ts
+++ src/app/services/ReportingService.ts
@@ -3,16 +3,15 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { VehicleModel } from '../models/VehicleModel';
 import { MonthlySalesRequest } from '../models/MonthlySalesRequest';
+import { TestVehicleModel, TestVehicleRequest, TestVehicleResponse } from '../models/TestVehicleModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportingService {
 
-  // private baseUrl = 'http://localhost:8080/api';
+  //private baseUrl = 'http://localhost:8081/api';
   private baseUrl = 'https://fleet-manager.in/api'; // Production URL
-
-  //private baseUrl = 'http://fleet-manager-prd.us-east-2.elasticbeanstalk.com/api';
 
   constructor(private http: HttpClient) {}
 
@@ -20,9 +19,25 @@ export class ReportingService {
     return this.http.get<VehicleModel[]>(this.baseUrl);
   }
 
-
   getMonthlySalesReport(request: MonthlySalesRequest): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/analytics/monthly-sales`, request);
+  }
+
+  // Test Vehicle Report Methods
+  getTestVehicleReport(request: TestVehicleRequest): Observable<TestVehicleModel[]> {
+    return this.http.post<TestVehicleModel[]>(`${this.baseUrl}/analytics/test-vehicles`, request);
+  }
+
+  getTestVehicleSummary(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/vehicles/testDriveVehicles`);
+  }
+
+  exportTestVehicleReport(request: TestVehicleRequest, format: string): Observable<Blob> {
+    const params = new HttpParams().set('format', format);
+    return this.http.post<Blob>(`${this.baseUrl}/analytics/test-vehicles/export`, request, {
+      params: params,
+      responseType: 'blob' as 'json'
+    });
   }
 
 }
